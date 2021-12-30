e054fd47dcf489e732c3da71339e40586303014d
faugan <foganbidi@gmail.com>
2021-12-30 08:36:08 +0000

    chemin

--- pages/api/save.js
+++ pages/api/save.js
@@ -1,12 +1,16 @@
 const fs=require("fs");
+var path = require('path');
+
 var uuid = require('uuid');
 const save=(req,res)=>{
     const body=req.body;
     const name=uuid.v4();
-    fs.writeFile(`./public/${name}.json`,JSON.stringify({data:body}),(err)=>{
+    let chemin=path.join(__dirname,"../../public/"+name+"/json");
+    
+    fs.writeFile(chemin,JSON.stringify({data:body}),(err)=>{
         if(err){
             console.log("there is an error",err);
-            res.status(200).json({error:"404",err});
+            res.status(200).json({error:"404",err,chemin});
         }else{
             console.log("we are go on the sever",body)
             res.status(200).json({file:`${name}.json`});
